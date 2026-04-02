3e58ee214d3d9952fe8f76f9dcbb4c520838f0d6
electroreal1 <155461193+electroreal1@users.noreply.github.com>
2026-04-02 18:39:52 +0300

    fuck this shit

--- common/src/main/java/github/electro/mythos/Mythos.java
+++ common/src/main/java/github/electro/mythos/Mythos.java
@@ -1,9 +1,11 @@
 package github.electro.mythos;
 
+import github.electro.mythos.registry.SkillRegistry;
+
 public final class Mythos {
     public static final String MOD_ID = "Mythos"; //TODO replace template with your mod id
 
     public static void init() {
-        // Write common init code here.
+        SkillRegistry.init();
     }
 }
